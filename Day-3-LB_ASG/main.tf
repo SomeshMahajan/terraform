f491c5878f7ce255f9a7d876e0a88e9c0e61e22c
Pratiksha <pratikshafegade@gmail.com>
2026-09-04 21:15:05 +0530

    v1

--- Day-3-LB_ASG/main.tf
+++ Day-3-LB_ASG/main.tf
@@ -76,7 +76,7 @@ resource "aws_lb_listener" "listener" {
     load_balancer_arn = aws_lb.lb.arn 
     port = 80
     protocol = "HTTP"
-    default_actions {
+    default_action {
         type = "forward"
         target_group_arn = aws_lb_target_group.tg.arn 
     }
