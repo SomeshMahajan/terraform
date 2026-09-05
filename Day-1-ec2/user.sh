1831f79152c0dce2b5e8f2555596d8824fa8110a
Pratiksha <pratikshafegade@gmail.com>
2026-09-05 14:08:32 +0530

    v9

--- Day-1-ec2/user.sh
+++ Day-1-ec2/user.sh
@@ -3,6 +3,6 @@ sudo apt update -y
 sudo apt install nginx -y  
 sudo systemctl start nginx 
 sudo systemctl enable nginx 
-sudo rm -rf /usr/share/nginx/html/*
-sudo echo "<h1> Hello From $HOSTNAME </h1>" > /usr/share/nginx/html/index.html
+sudo rm -rf /var/www/html/*
+sudo echo "<h1> Hello From $HOSTNAME </h1>" > /var/www/html/index.html
 sudo systemctl restart nginx
