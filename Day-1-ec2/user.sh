e4ed474b849c7cff9b5b5ac4a8322140800a09b0
Pratiksha <pratikshafegade@gmail.com>
2026-09-05 12:16:11 +0530

    v5

--- Day-1-ec2/user.sh
+++ Day-1-ec2/user.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 dnf update -y
 dnf install nginx -y
-systemctl start nginx 
-systemctl enable nginx 
+sudo systemctl start nginx 
+sudo systemctl enable nginx  
+sudo rm -rf /usr/share/nginx/html/*
+sudo echo "<h1> Hello From $HOSTNAME </h1>" > /usr/share/nginx/html/index.html
+sudo systemctl restart nginx
